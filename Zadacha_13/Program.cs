756703e9c761e50e309652ba8225fd9d53761f96
Dragula <elegantmorpeh@icloud.com>
2023-02-02 00:26:43 +0300

    Complete

---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+System.Console.WriteLine("Введите любое число: ");
+int n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+if (n < 100)
+{
+System.Console.WriteLine(" третьей цифры нет");
+}
+else
+{
+for (; n >= 1000; n /= 10);
+System.Console.WriteLine( n % 10);
+}
---
+++ Zadacha_15/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите числоа от 1 до 7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+switch (num)
+{
+case 1: case 2: case 3: case 4: case 5:
+System.Console.WriteLine("Нет");
+break;
+case 6: case 7:
+System.Console.WriteLine("Да");
+break;
+}
